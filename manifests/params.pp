e2ca8a983e1bd8798b34f59477bf1028e67f9589
Kevin Chang <kckaiwei@gmail.com>
2016-07-12 09:09:45 -0500

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,5 @@
 #Default parameters are located here
 
-class hprest::params {
-  $ilo_username   = "username"
-  $ilo_password   = "password"
-  $ilo_ip         = "10.0.0.100"
-  $hprest_command = ""
+class wpython::params {
+  $version   = "2.7.12"
 }
